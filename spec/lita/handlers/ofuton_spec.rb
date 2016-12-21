01b06d234b6fba0491fd8fed62f066763ece68e2
gouf <innocent.zero@gmail.com>
2016-12-21 23:10:54 +0900

    Add spec

--- spec/lita/handlers/ofuton_spec.rb
+++ spec/lita/handlers/ofuton_spec.rb
@@ -1,4 +1,17 @@
 require "spec_helper"
 
+# In development environment, adjust redis host/port setting if you using docker or whatever
+# Lita.configure do |config|
+#   config.redis[:host] = 'localhost'
+#   config.redis[:port] = '6379'
+# end
+
 describe Lita::Handlers::Ofuton, lita_handler: true do
+  let(:source) { ::Lita::Source.new(user: user) }
+  let(:user) { ::Lita::User.create('U02FERE10', name: 'foo') }
+
+  it 'response includes ｵｽﾄﾝ' do
+    send_message('ofuton')
+    expect(replies.join("\n")).to match('ｵｽﾄﾝ')
+  end
 end
