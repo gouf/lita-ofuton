77e2ea926df2fb0a164c4c9a462a0a7f4339afa6
gouf <innocent.zero@gmail.com>
2016-12-21 23:10:16 +0900

    Add lib requirements

--- lib/lita-ofuton.rb
+++ lib/lita-ofuton.rb
@@ -1,4 +1,7 @@
 require "lita"
+require 'slack-ruby-client'
+require_relative 'ofuton/shindan_scrape'
+require_relative 'ofuton/ofuton'
 
 Lita.load_locales Dir[File.expand_path(
   File.join("..", "..", "locales", "*.yml"), __FILE__
